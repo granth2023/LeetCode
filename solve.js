c95b2602502141483890db90ef525547aa50b0cf
granth2023 <grantharris38@gmail.com>
2024-01-15 15:55:16 -0500

    function two arrays

--- solve.js
+++ solve.js
@@ -1,3 +1,5 @@
 //complete twoArrays function
 //return a string of yes or no 
 //function accepts following parameters: Integer K, Integer Array A, Integer array B 
+
+function twoArrays(k, a, b) {
