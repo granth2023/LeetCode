a9bb44f32c5a047b5f1f5cdf3fa861757c214166
granth2023 <grantharris38@gmail.com>
2024-01-15 16:00:23 -0500

    coment about loops

--- solve.js
+++ solve.js
@@ -5,14 +5,13 @@
 function twoArrays(k, a, b) {
 
 //need to find if a version of A lines up with a version of B  such that they add up to K 
+//need to loop through both versions of the arrays because just because they match at an iindex that could throw the whole thing off. so we need a way to get all the variations of the a loop and then send the b loop through that. 
+//
 
-
-if (a[i] + b[i] === K){
+if (a[i] + b[i] === k){
     return 'yes'
 } else {
     return 'no';
 }
-
-
-    return 
+ 
 }
