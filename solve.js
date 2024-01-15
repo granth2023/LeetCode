9b1f33779fdfefa132ba8da55b88aac68a59159d
granth2023 <grantharris38@gmail.com>
2024-01-15 16:02:49 -0500

    sorting method

--- solve.js
+++ solve.js
@@ -6,7 +6,7 @@ function twoArrays(k, a, b) {
 
 //need to find if a version of A lines up with a version of B  such that they add up to K 
 //need to loop through both versions of the arrays because just because they match at an iindex that could throw the whole thing off. so we need a way to get all the variations of the a loop and then send the b loop through that. 
-//
+//sorting them both gives us the best chance to see if the numbers compliment one another 
 
 if (a[i] + b[i] === k){
     return 'yes'
